a60191d649076f2313ee4ca76ac0ac0b13ea72ed
Ryan Closner <ryan@ryanclosner.com>
2013-10-07 09:24:45 -0700

    Log rollback response

--- db/migrate.js
+++ db/migrate.js
@@ -7,5 +7,5 @@ console.log('Migrating...\n');
 exec(process.cwd() + '/node_modules/db-migrate/bin/db-migrate up --config config/database.json --migrations-dir db/migrations -e "$NODE_ENV"', function (err, res) {
   if (err) throw(err)
   console.log(res);
-  console.log('Database migrated successfully.');
+  console.log('Database migrated successfully.\n');
 });
--- db/rollback.js
+++ db/rollback.js
@@ -4,6 +4,8 @@ var exec = require('child_process').exec;
 
 console.log('Rolling back...');
 
-exec(process.cwd() + '/node_modules/db-migrate/bin/db-migrate down --config config/database.json --migrations-dir db/migrations -e "$NODE_ENV"', function () {
-  console.log('Database rolled back successfully.');
+exec(process.cwd() + '/node_modules/db-migrate/bin/db-migrate down --config config/database.json --migrations-dir db/migrations -e "$NODE_ENV"', function (err, res) {
+  if (err) throw(err);
+  console.log(res)
+  console.log('Database rolled back successfully.\n');
 });
