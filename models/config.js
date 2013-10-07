0b71cacbe8cc918a70a77fbdcc45405191775492
Ryan Closner <ryan@ryanclosner.com>
2013-10-07 09:02:42 -0700

    Fix database configuration bug

--- models/config.js
+++ models/config.js
@@ -1,4 +1,18 @@
-var database = require(process.cwd() + '/config/database');
+var dbConfig = require(process.cwd() + '/config/database')[process.env.NODE_ENV];
+var database = {}
+
+var dbConfigKeys = Object.keys(dbConfig);
+
+for (var i = 0; i < dbConfigKeys.length; i++) {
+  var key = dbConfigKeys[i];
+  var val = dbConfig[key];
+
+  if (typeof(val) === 'object' && typeof(val.ENV) !== 'undefined') {
+    database[key] = process.env[val.ENV];
+  } else {
+    database[key] = val;
+  }
+}
 
 var Config = {
   database: database
